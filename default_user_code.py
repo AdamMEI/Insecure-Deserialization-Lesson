c7bebc066c5e8c9ba108a978b4ea997517dd64b7
AdamMEI <aimeilicke@gmail.com>
2025-04-13 10:52:14 -0700

    Added most of the instructional text

--- default_user_code.py
+++ default_user_code.py
@@ -9,13 +9,13 @@ class Comment:
 
 # Save comments (serialize)
 def save_comments(comments):
-    cookie_manager.save_cookie("comments", base64.b64encode(pickle.dumps(comments)).decode("utf-8"))
+    encoded_comments = base64.b64encode(pickle.dumps(comments)).decode("utf-8")
+    cookie_manager.save_cookie("comments", encoded_comments)
 
 # Load comments (deserialize)
 def load_comments():
-    cookie = base64.b64decode(cookie_manager.get_cookie("comments"))
-    if cookie:
-        return pickle.loads(cookie)
+    cookie = cookie_manager.get_cookie("comments")
+    decoded_cookie = base64.b64decode(cookie)
+    if decoded_cookie:
+        return pickle.loads(decoded_cookie)
     return []
-
-#print(base64.b64encode(pickle.dumps([Comment("I liked it!", "Sharon"), Comment("I thought the ending was bad.", "Jeff")])).decode("utf-8"))
